c2abcf4bb8a60e5a0ae7e31364546298d915851b
AlessioRomano95 <romanalex400@gmail.com>
2024-03-19 16:02:04 +0100

    [ADD] js & css

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('js ok')
+
+const {createApp} = Vue 
+const api = 'https://flynn.boolean.careers/exercises/api/random/mail'
+const app = createApp({
+    data: () => ({
+        usersEmail: [],
+    }),
+    created(){
+        for(let i = 0; i < 10; i++) {
+            axios.get(api).then((res) => {
+                this.usersEmail.push(res.data.response)
+            })
+        }
+    }
+})
+app.mount('#root')
